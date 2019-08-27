1de38ef144a9cd13457f1f716635da242b4fab39
J Roberto Olivas <direccion@expendo.mx>
2019-08-27 16:21:47 -0600

    Code examples for NuGet In-House Succinctly

---
+++ Chapter3/myestensionmethods/myestensionmethods/MyExtensionMethods.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace myestensionmethods
+{
+    public static class MyExtensionMethods
+    {
+        /// <summary>
+        /// Message posted when the user presses the left mouse button while
+        /// the cursor is within the nonclient area of a window. The window
+        /// which has the cursor receives the message.
+        /// </summary>
+        private const int WM_NCLBUTTONDOWN = 0x00A1;
+
+        /// <summary>
+        /// Indicates that the screen coordinates belong to the
+        /// title bar.
+        /// </summary>
+        private const int HTCAPTION = 2;
+
+        /// <summary>
+        /// Creates a WM_NCLBUTTONDOWN message for the specified form. This
+        /// method provides a mechanism to drag the form from anywhere, including the
+        /// title bar, if there is one.
+        /// </summary>
+        /// <param name="targetForm">The form instance which will receive the message</param>
+        /// <returns></returns>
+        public static Message CreateWmNclButtonDown(this Form targetForm) => Message.Create(targetForm.Handle, WM_NCLBUTTONDOWN, new IntPtr(HTCAPTION), IntPtr.Zero);
+
+        /// <summary>
+        /// Draw the hands for an analog clock face in a client area (usually a form's client area).
+        /// </summary>
+        /// <param name="graphicsInstance">Graphics object asociated to the client area.</param>
+        /// <param name="clientSize">Width and Height of the client area.</param>
+        /// <param name="handsColor">Color used to draw the hour and minute hands.</param>
+        /// <param name="secondHandColor">Color used to draw the seconds hand.</param>
+        public static void DrawClockHands(this Graphics graphicsInstance, Size clientSize,Color handsColor, Color secondHandColor)
+        {
+            using (var handsPen = new Pen(handsColor, 4))
+            {
+                // Get the hour and the minute including any fraction elapsed.
+                DateTime currentDateTime = DateTime.Now;
+                var currentHour = currentDateTime.Hour + currentDateTime.Minute / 60f + currentDateTime.Second / 3600f;
+                var currentMinute = currentDateTime.Minute + currentDateTime.Second / 60f;
+
+                // Gets the center point for the clock face
+                var clockFaceCenter = new PointF(0, 0);
+
+                //Set the scale factor for drawing the hour hand.
+                var hourHandXFactor = 0.2f * clientSize.Width;
+                var hourHandYFactor = 0.2f * clientSize.Height;
+
+                //Gets the angle for the current hour.
+                var hourHandAngle = -Math.PI / 2 + 2 * Math.PI * currentHour / 12.0;
+
+                //Gets the hour hand end point
+                var hourHandEndPoint = new PointF((float)(hourHandXFactor * Math.Cos(hourHandAngle)),(float)(hourHandYFactor * Math.Sin(hourHandAngle)));
+
+                //Assing the hands color to the drawing pen
+                handsPen.Color = handsColor;
+
+                //Draws the hour hand
+                graphicsInstance.DrawLine(handsPen, hourHandEndPoint, clockFaceCenter);
+
+                //Set the scale factor for the minute hand
+                float minuteHandXFactor = 0.3f * clientSize.Width;
+                float minuteHandYFactor = 0.3f * clientSize.Height;
+
+                //Gets the angle for the current minute
+                double minuteHandAngle = -Math.PI / 2 + 2 * Math.PI * currentMinute / 60.0;
+
+                //Gets the endpoint for the minute hand
+                var minuteHandEndPoint = new PointF((float)(minuteHandXFactor * Math.Cos(minuteHandAngle)), (float)(minuteHandYFactor * Math.Sin(minuteHandAngle)));
+
+                //Sets the pen width to 2 pixels for minute hand
+                handsPen.Width = 2;
+
+                //Draws the minute hand.
+                graphicsInstance.DrawLine(handsPen, minuteHandEndPoint, clockFaceCenter);
+
+                //Set the scale factor for second hand
+                var secondHandXFactor = 0.4f * clientSize.Width;
+                var secondHadYFactor = 0.4f * clientSize.Height;
+
+                //Gets the angle for the current second
+                var second_angle = -Math.PI / 2 + 2 * Math.PI * currentDateTime.Second / 60.0;
+
+                //Gets the end point for the second hand
+                var secondEndPoint = new PointF((float)(secondHandXFactor * Math.Cos(second_angle)),(float)(secondHadYFactor * Math.Sin(second_angle)));
+
+                //Sets the color for the second hand
+                handsPen.Color = secondHandColor;
+                handsPen.Width = 1;
+
+                //Draws the second hand.
+                graphicsInstance.DrawLine(Pens.Red, secondEndPoint, clockFaceCenter);
+            }
+        }
+
+        /// <summary>
+        /// Draws an analog clock face in a client area (usually a form's client area).
+        /// </summary>
+        /// <param name="graphicsInstance">Graphics object asociated to the client area.</param>
+        /// <param name="clientSize">Width and Height of the client area.</param>
+        /// <param name="penColor">Color used to draw the analog clock face.</param>
+        /// <param name="penWidth">Width of the pen used to draw the analog clock face.</param>
+        public static void DrawClockFace(this Graphics graphicsInstance, Size clientSize, Color penColor, int penWidth)
+        {
+            using (var clockPen = new Pen(penColor, penWidth))
+            {
+                //Draws the clock face's outline
+                graphicsInstance.DrawCircle(clientSize, penColor, penWidth);
+
+                //Draws the tick marks around the clock's face outline
+
+                //Defines a round cap for beginning and ending of every line
+                clockPen.StartCap = LineCap.Round;
+                clockPen.EndCap = LineCap.Round;
+
+                //Defines the scale factors used to draw
+                //the tick marks around the clock's face
+
+                //Scale factors for the mark's outer point (x,y)
+                var outXFactor = 0.45f * clientSize.Width;
+                var outYFactor = 0.45f * clientSize.Height;
+
+                //Scale factors for the mark's inner point (x,y)
+                var innXFactor = 0.425f * clientSize.Width;
+                var innYFactor = 0.425f * clientSize.Height;
+
+                //Scale factors for the hour mark's inner point (x,y)
+                var hourXFactor = 0.4f * clientSize.Width;
+                var hourYFactor = 0.4f * clientSize.Height;
+
+                //Loop for iterating the hour's minutes
+                for (int minute = 1; minute <= 60; minute++)
+                {
+                    //Calculates the angle for every minute
+                    var minuteAngle = Math.PI * minute / 30.0;
+                    var cosineAngle = (float)Math.Cos(minuteAngle);
+                    var sineAngle = (float)Math.Sin(minuteAngle);
+
+                    //Calculates inner and outer points
+                    //for the current tick mark
+                    var innerPoint = (minute % 5 == 0) ? new PointF(hourXFactor * cosineAngle,hourYFactor * sineAngle) : new PointF(innXFactor * cosineAngle, innYFactor * sineAngle);
+                    var outerPoint = new PointF(outXFactor * cosineAngle,outYFactor * sineAngle);
+
+                    graphicsInstance.DrawLine((minute % 5 == 0) ? clockPen : Pens.Black,innerPoint,outerPoint);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a circle within a client area (usually a form's client area).
+        /// </summary>
+        /// <param name="graphicsInstance">Graphics object asociated to the client area.</param>
+        /// <param name="clientSize">Width and Height of the client area.</param>
+        /// <param name="penColor">Color used to draw the circle.</param>
+        /// <param name="penWidth">Width of the pen used to draw the circle.</param>
+        public static void DrawCircle(this Graphics graphicsInstance,Size clientSize,Color penColor, int penWidth)
+        {
+            using (var circlePen = new Pen(penColor, penWidth))
+            {
+                graphicsInstance.DrawEllipse(circlePen, -clientSize.Width / 2, -clientSize.Height / 2, clientSize.Width, clientSize.Height);
+            }
+        }
+
+        /// <summary>
+        /// Builds a context menu based on the elements of a Dictionary.
+        /// </summary>
+        /// <param name="contextMenu">Instance of the context menu to build.</param>
+        /// <param name="menuItems">Dictionary with all menu items. The key is used in the Tag property to identify the item</param>
+        /// <param name="clickEventHandler">Handler for the Click event of every item</param>
+        public static void BuildContextMenu(this ContextMenuStrip contextMenu, Dictionary<string,string> menuItems, EventHandler clickEventHandler)
+        {
+            menuItems.ToList().ForEach(item =>
+            {
+                var menuItem = new ToolStripMenuItem
+                {
+                    Text = item.Value,
+                    Tag = item.Key
+                };
+                menuItem.Click += clickEventHandler;
+
+                contextMenu.Items.Add(menuItem);
+            });
+        }
+
+    }
+}
---
+++ Chapter5/MyAnalogClock/MyAnalogClock/MyAnalogClockForm.cs
@@ -0,0 +1,188 @@
+﻿using myextensionmethods;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace MyAnalogClock
+{
+    /// <summary>
+    /// Displays an analog clock on the screen
+    /// </summary>
+    public partial class MyAnalogClockForm : Form
+    {
+        /// <summary>
+        /// Instance of the context menu which allows us
+        /// to exit the application.
+        /// </summary>
+        private ContextMenuStrip _contextMenu;
+
+        /// <summary>
+        /// Dictionary with the form's context menu items.
+        /// The key is assigned to the Tag property of the ToolStripMenuItem
+        /// </summary>
+        private readonly Dictionary<string, string> _contextMenuItems = new Dictionary<string, string>
+        {
+            {"EXIT","Exit"}
+        };
+
+        /// <summary>
+        /// Instance of the timer in charge of updating the clock.
+        /// </summary>
+        private Timer _clockTimer;
+
+        /// <summary>
+        /// Creates the instance of the form.
+        /// </summary>
+        public MyAnalogClockForm()
+        {
+            InitializeComponent();
+
+            //Subscribes the Paint event of the form
+            Paint += MyAnalogClockForm_Paint;
+
+            //Subscribes the Load event of the form
+            Load += MyAnalogClockForm_Load;
+
+            //Subscribes the MouseDown event of the form
+            //in order to allow dragging it
+            MouseDown += MyAnalogClockForm_MouseDown;
+
+        }
+
+        /// <summary>
+        /// Handles the Load event of the form.
+        /// </summary>
+        /// <param name="sender">Instance of the form that raises the event.</param>
+        /// <param name="e">Instance of <see cref="EventArgs"/> which holds the data for managing the event.</param>
+        private void MyAnalogClockForm_Load(object sender, EventArgs e)
+        {
+            //Removes title bar and border
+            //from the form.
+            Text = string.Empty;
+            FormBorderStyle = FormBorderStyle.None;
+            ControlBox = false;
+
+            //Creates the instance of the context menu
+            _contextMenu = new ContextMenuStrip();
+
+            //Builds the context menu using the items dictionary.
+            //Subscribes the Click event for every item to the MenuItem_Click method.
+            _contextMenu.BuildContextMenu(_contextMenuItems, MenuItem_Click);
+
+            // Attach the context menu to the form.
+            ContextMenuStrip = _contextMenu;
+
+            // Set the size to a square of 300x300.
+            SetSize(300, 300);
+
+            //Use double buffering to avoid flickering.
+            DoubleBuffered = true;
+
+            //The form will not be shown in the task bar.
+            ShowInTaskbar = false;
+
+            // Set focus to the form. You should do this
+            // in order to make Alt+F4 close the clock properly.
+            Focus();
+
+            //Creates an instance of a timer
+            //and adjusts the time for firing the
+            //Tick event (1000 milliseconds == 1 second)
+            _clockTimer = new Timer
+            {
+                Interval = 1000
+            };
+
+            //Subscribes the Tick event for the timer instance
+            _clockTimer.Tick += _clockTimer_Tick;
+            _clockTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Handles the MouseDown event of the form, in order to
+        /// allow dragging from anywhere of the clock's surface.
+        /// </summary>
+        /// <param name="sender">Instance of the form that raises the event.</param>
+        /// <param name="e">Instance of <see cref="EventArgs"/> which holds the data for managing the event.</param>
+        private void MyAnalogClockForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            //If we hold the mouse left button we can drag the clock.
+            if (e.Button == MouseButtons.Left)
+            {
+                // Release the mouse capture started by the mouse down.
+                Capture = false;
+
+                //Create the windows message
+                var wMessage = this.CreateWmNclButtonDown();
+
+                //Sends the message to the form's instance
+                DefWndProc(ref wMessage);
+            }
+        }
+
+        // Redraws the clock to show the current hand position.
+        private void _clockTimer_Tick(object sender, EventArgs e) => Refresh();
+
+        /// <summary>
+        /// Handles the Paint event of the form, and draws the clock.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyAnalogClockForm_Paint(object sender, PaintEventArgs e)
+        {
+            //Clear the drawing surface of the form
+            //and fills it with the form's BackColor
+            e.Graphics.Clear(BackColor);
+
+            //Sets the quality for graphics rendering
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            //Sets the quality for text rendering
+            e.Graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+
+            //Translates the graphics coordinates to
+            //center the drawing.
+            e.Graphics.TranslateTransform(ClientSize.Width / 2, ClientSize.Height / 2);
+
+            // Draws the clock face including tick marks.
+            e.Graphics.DrawClockFace(ClientSize, Color.Blue, 4);
+
+            // Draws the center of the clock.
+            // This code was implemented in myextensionmethdos
+
+            // Draws the clock hands.
+            e.Graphics.DrawClockHands(ClientSize, Color.Red, Color.OrangeRed);
+        }
+
+        /// <summary>
+        /// Handles the click event for every item in the context menu.
+        /// Since there is only one item (Exit), executes the Close method
+        /// to close the form and exit the application.
+        /// </summary>
+        /// <param name="sender">Instance of the menu item which raises the event.</param>
+        /// <param name="e">Instance of <see cref="EventArgs"/> which holds the data for managing the event.</param>
+        private void MenuItem_Click(object sender, EventArgs e) => Close();
+
+        /// <summary>
+        /// Set the form's size.
+        /// </summary>
+        /// <param name="clientWidth">Form's width in pixels.</param>
+        /// <param name="clientHeight">Form's height in pixels.</param>
+        private void SetSize(int clientWidth, int clientHeight)
+        {
+            //Set the form's size.
+            ClientSize = new Size(clientWidth, clientHeight);
+
+            // Set the form's region to a circle.
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(ClientRectangle);
+            Region = new Region(path);
+
+            // Redraw the form.
+            Refresh();
+        }
+    }
+}
